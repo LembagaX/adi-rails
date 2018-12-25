d6c3388856f34155fd96e45f48af507729219597
Agung Gunawan <lembagax@gmail.com>
2018-12-25 05:24:12 -0800

    Preflight for PUT and PATCH

--- config/application.rb
+++ config/application.rb
@@ -34,7 +34,7 @@ module AdiService
     config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins '*'
-        resource '*', headers: :any, methods: [:get, :post, :options, :delete]
+        resource '*', headers: :any, methods: [:get, :post, :options, :delete, :put, :patch]
       end
     end
   end
